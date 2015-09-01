b7f46854d5e31a7918131e3d3ba5696425927ec3
YujinChu <buc0809@naver.com>
2015-09-01 19:06:02 +0900

    ...

---
+++ src/com/company/t08_inheritance/InheritanceTest.java
@@ -0,0 +1,37 @@
+package com.company.t08_inheritance;
+
+/**
+ * Created by Mirim on 2015-09-01.
+ */
+public class InheritanceTest {
+    public static void main(String[] args){
+        class Parent{
+            public String read(){
+                return "Parnet ??ве?ве?";
+            }
+        }
+
+        class FirstChild extends Parent{
+            public String read(){
+                return super.read()+": firstChild";
+            }
+        }
+
+        class SecondChild extends Parent{
+            public String read(){
+                return super.read()+": secondChild";
+            }
+        }
+
+        class ThirdChild extends Parent{
+            Parent p;
+            public ThirdChild(Parent p){
+                this.p = p;
+            }
+
+            public String read(){
+                return p.read()+": thirdChild";
+            }
+        }
+    }
+}

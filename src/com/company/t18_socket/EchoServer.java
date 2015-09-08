8c2f8cddc1195ab594f1fddaf05b560dc4f806eb
YujinChu <buc0809@naver.com>
2015-09-08 20:49:10 +0900

    ...

--- src/com/company/t18_socket/EchoServer.java
+++ src/com/company/t18_socket/EchoServer.java
@@ -1,5 +1,6 @@
 package com.company.t18_socket;
 
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,6 +17,24 @@ public class EchoServer {
             Socket socket = server.accept();
             InetAddress address = socket.getInetAddress();
             System.out.println(address.getHostAddress()+" 로부터 접속했습니다.");
+
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(in));
+            PrintWriter pw =
+                    new PrintWriter(new OutputStreamWriter(out));
+
+            String line = null;
+            while((line = br.readLine()) != null){
+                System.out.println("클라이언트에서 전송 받은 문자열"+line);
+                pw.println(line);
+                pw.flush();
+            }
+            pw.close();
+            br.close();
+            socket.close();
 
         }catch (Exception e){
             e.printStackTrace();

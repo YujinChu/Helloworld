723cdea1d9d6e510a536bf55c4e064901c5e3d36
YujinChu <buc0809@naver.com>
2015-09-15 18:57:28 +0900

    ...

--- src/com/company/t19_multithreadsocket/EchoThreadServer.java
+++ src/com/company/t19_multithreadsocket/EchoThreadServer.java
@@ -1,27 +1,24 @@
 package com.company.t19_multithreadsocket;
 
-import java.io.IOException;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by Mirim on 2015-09-10.
  */
 public class EchoThreadServer {
-    try{
-        ServerSocket server = null;
-        try {
-            server = new ServerSocket(10000);
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        System.out.println("立加措扁....");
+    public static void main(String[] args){
+        try{
+            ServerSocket server = new ServerSocket(10000);
+            System.out.println("立加措扁....");
 
-        while (true){
-            Socket socket = server.accept();
-            EchoThread th = new EchoThread(socket);
-            th.start();
+            while (true){
+                Socket socket = server.accept();
+                EchoThread th = new EchoThread(socket);
+                th.start();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
-    } catch (Exception e){
-        e.printStackTrace();
     }
 }
---
+++ src/com/company/t20_chatserver/ChatServer.java
@@ -0,0 +1,19 @@
+package com.company.t20_chatserver;
+
+import java.net.ServerSocket;
+
+/**
+ * Created by Mirim on 2015-09-15.
+ */
+public class ChatServer {
+    public static void main(String[] args){
+        try {
+            System.out.println("??¨ù? ¢¥?¡¾?...");
+            ServerSocket server = new ServerSocket(10000);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
